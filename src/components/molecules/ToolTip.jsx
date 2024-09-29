9b80ac51f32aa2598a42ae43d16dbe348356360c
Sushil <bajracharya.sushil01@gmail.com>
2024-09-29 13:46:03 +0545

    refactor: class to className

--- src/components/molecules/ToolTip.jsx
+++ src/components/molecules/ToolTip.jsx
@@ -10,7 +10,7 @@ export default function ToolTip({ children, text }) {
         className="absolute flex items-center z-50 w-36 right-[-140px] top-2 group-hover:opacity-100 delay-300 
          transition-opacity opacity-0"
       >
-        <div class="h-0 w-0 border-4 border-y-transparent border-l-transparent border-gray-800 "></div>
+        <div className="h-0 w-0 border-4 border-y-transparent border-l-transparent border-gray-800 "></div>
         <span
           className="
         bg-gray-800 p-1 text-sm text-gray-100 rounded-md"
