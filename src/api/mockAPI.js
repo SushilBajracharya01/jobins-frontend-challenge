6865128a2b927a6d76fd3262c2bbb9b5669ce4f6
Sushil <bajracharya.sushil01@gmail.com>
2024-09-29 12:09:10 +0545

    feat: order mock api add

--- src/api/mockAPI.js
+++ src/api/mockAPI.js
@@ -1,13 +1,62 @@
 import MockAdapter from "axios-mock-adapter";
 import { ORDERS } from "./_mock_data";
-import axios from "axios";
+import { callAPI } from "./apiInstance";
+import {
+  getParams,
+  isAfterOrSameDay,
+  isBeforeOrSameDay,
+} from "../utils/helpers";
 
-const mock = new MockAdapter(axios);
+const mock = new MockAdapter(callAPI, { delayResponse: 600 });
 
-mock.onGet("/orders").reply(200, {
-  count: ORDERS.length,
-  message: "Orders fetched successfully",
-  results: ORDERS,
+const usersUri = "/orders";
+const url = new RegExp(`${usersUri}*`);
+
+mock.onGet(url).reply(function (config) {
+  const params = getParams(config.url); // offset: 0, limit: 10
+
+  let data = [...ORDERS];
+
+  if (params.status !== "All") {
+    data = data.filter((d) => d.status === params.status);
+  }
+  if (params.search) {
+    data = data.filter(
+      (d) =>
+        d.customer.toLowerCase().includes(params.search.toLowerCase()) ||
+        d.id.toString().includes(params.search)
+    );
+  }
+  if (params.from) {
+    data = data.filter((d) =>
+      isAfterOrSameDay(new Date(d.date), new Date(params.from))
+    );
+  }
+  if (params.to) {
+    data = data.filter((d) =>
+      isBeforeOrSameDay(new Date(d.date), new Date(params.to))
+    );
+  }
+
+  return [
+    200,
+    {
+      count: data.length,
+      message: "Orders fetched successfully",
+      results: data.slice(params.offset, params.limit),
+    },
+  ];
 });
 
-export const fetchOrders = () => axios.get("/orders");
+export const fetchOrders = (filters) => {
+  const searchParams = new URLSearchParams();
+
+  if (filters?.offset) searchParams.append("offset", filters.offset);
+  if (filters?.limit) searchParams.append("limit", filters.limit);
+  if (filters?.status) searchParams.append("status", filters.status);
+  if (filters?.search) searchParams.append("search", filters.search);
+  if (filters?.from) searchParams.append("from", filters.from);
+  if (filters?.to) searchParams.append("to", filters.to);
+
+  return callAPI.get(`/orders?${searchParams}`);
+};
